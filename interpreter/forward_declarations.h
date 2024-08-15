e5e13ce3b34aa4523a680b32084f9e1cca19dfe7
skits4107 <74423228+skits4107@users.noreply.github.com>
2024-08-15 10:37:07 -0700

    started refactor

---
+++ interpreter/forward_declarations.h
@@ -0,0 +1,36 @@
+#pragma once
+
+//visitors
+class Visitor;
+class PrintVisitor;
+class EvalVisitor;
+
+//nodes
+class Node;
+class IntLiteralNode;
+class FloatLiteralNode;
+class DoubleLiteralNode;
+class CharLiteralNode;
+class StringLiteralNode;
+class BoolLiteralNode;
+class ParamterNode;
+class CodeBlockNode;
+class FuncDecNode;
+class ExpressionNode;
+class TypeCastNode;
+class LogicalNotNode;
+class AssignmentStatementNode;
+class FuncCallStatementNode;
+class ConditionStatementNode;
+class ForLoopNode;
+class WhileLoopNode;
+class ReturnStatementNode;
+class BreakStatementNode;
+class ContinueStatementNode;
+class ArrayIndexingNode;
+class ArrayGetElementNode;
+class IdentifierNode;
+class ProgramNode;
+class ArrayAssignmentNode;
+
+class IncDecStatementNode;
