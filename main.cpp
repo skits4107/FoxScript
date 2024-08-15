402eaf6649f0a20bec7df6272880b0146a7fb35e
skits4107 <74423228+skits4107@users.noreply.github.com>
2024-08-15 16:32:59 -0700

    added command line argument parsing

--- main.cpp
+++ main.cpp
@@ -4,23 +4,39 @@
 #include "interpreter/lexer/lexer.h"
 #include "interpreter/parser/parser.h"
 #include <fstream>
+#include <unistd.h>
 
 int main(int argc, char** argv){
 
-    //TODO: add future argument checks.
+
     if (argc < 2){
         std::cerr << "Error: Invalid number of arguments" << std::endl;
-        return -1;
+        return 1;
     }
 
-
-
     std::string fileName = argv[1];
+    bool shouldPrintDebug = false;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "f:d")) != -1) {
+        switch (opt) {
+            case 'f':
+                fileName = optarg;
+                break;
+            case 'd':
+                shouldPrintDebug = true;
+                break;
+            default:
+                std::cerr << "Error: Unkown argument "<< static_cast<char>(optopt) << std::endl;
+                return 1;
+        }
+    }
+
     std::ifstream file(fileName);
     
     if (!file.is_open()){
          std::cerr << "Error: invalid file " << fileName << std::endl;
-         return -1;
+         return 1;
     }
 
     std::string input = "";
@@ -28,26 +44,29 @@ int main(int argc, char** argv){
     while (!file.eof()){
         input += file.get(); //get every character in file
     }
-    //display file content (debugging)
-    std::cout << input << std::endl;
 
     //lexer converts string into tokens
     Lexer lexer;
     std::vector<Token> tokens = lexer.lex(input);
 
-    //diplay tokens (debugging)
-    for (int i=0; i<tokens.size(); i++){
-        std::cout << "Token(" << tokens[i].type << ", " << tokens[i].text << ", " << tokens[i].startPos << ')'<< std::endl;
-    }
 
     //parse tokens into AST 
     Parser parser(tokens);
     std::unique_ptr<ProgramNode> node = parser.parse();
 
-    //print out AST
-    PrintVisitor printVisitor;
-    node->accept(printVisitor);
-    
+    if (shouldPrintDebug){
+        //display file content (debugging)
+        std::cout << input << std::endl;
+
+        //diplay tokens (debugging)
+        for (int i=0; i<tokens.size(); i++){
+            std::cout << "Token(" << tokens[i].type << ", " << tokens[i].text << ", " << tokens[i].startPos << ')'<< std::endl;
+        }
+
+        //print out AST
+        PrintVisitor printVisitor;
+        node->accept(printVisitor);
+    }
 
     return 0;
 }
