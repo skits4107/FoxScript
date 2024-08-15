f795c014862ef0af5e792aa9e180a255f290c5a5
skits4107 <74423228+skits4107@users.noreply.github.com>
2024-08-15 16:07:46 -0700

    changed to using guard clauses and comments

--- main.cpp
+++ main.cpp
@@ -7,36 +7,44 @@
 
 int main(int argc, char** argv){
 
-    if (argc >= 2){
-        std::string fileName = argv[1];
-        
-        std::ifstream file(fileName);
-
-        std::string input = "";
-        
-        if (file.is_open()){
-            while (!file.eof()){
-                input += file.get(); //get every character in file
-            }
-            std::cout << input << std::endl;
-            Lexer lexer;
-            std::vector<Token> tokens = lexer.lex(input);
-            for (int i=0; i<tokens.size(); i++){
-                std::cout << "Token(" << tokens[i].type << ", " << tokens[i].text << ", " << tokens[i].startPos << ')'<< std::endl;
-            }
-
-            Parser parser(tokens);
-            std::unique_ptr<ProgramNode> node = parser.parse();
-            PrintVisitor printVisitor;
-            node->accept(printVisitor);
-        }
-        else{
-            std::cerr << "Error: invalid file " << fileName << std::endl;
-        }
-    }
-    else{
+    //TODO: add future argument checks.
+    if (argc < 2){
         std::cerr << "Error: Invalid number of arguments" << std::endl; 
     }
+
+    std::string fileName = argv[1];
+        
+    std::ifstream file(fileName);
+    
+    if (!file.is_open()){
+         std::cerr << "Error: invalid file " << fileName << std::endl;
+    }
+
+    std::string input = "";
+
+    while (!file.eof()){
+        input += file.get(); //get every character in file
+    }
+    //display file content (debugging)
+    std::cout << input << std::endl;
+
+    //lexer converts string into tokens
+    Lexer lexer;
+    std::vector<Token> tokens = lexer.lex(input);
+
+    //diplay tokens (debugging)
+    for (int i=0; i<tokens.size(); i++){
+        std::cout << "Token(" << tokens[i].type << ", " << tokens[i].text << ", " << tokens[i].startPos << ')'<< std::endl;
+    }
+
+    //parse tokens into AST 
+    Parser parser(tokens);
+    std::unique_ptr<ProgramNode> node = parser.parse();
+
+    //print out AST
+    PrintVisitor printVisitor;
+    node->accept(printVisitor);
+    
 
     return 0;
 }
